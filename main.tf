d993097c3ed89cbd4c19ed4f0321db1c5a7a4cf0
kodekloud-terraform-demo-user <96228864+kodekloud-terraform-demo-user@users.noreply.github.com>
2021-12-16 14:14:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+variable "ami" {}
+variable "region" {}
+variable "instance_type" {}
+
 resource "aws_instance" "terraform-cloud-demo-server" {
   ami           = var.ami
   instance_type = var.instance_type
