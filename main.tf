2059748198330bd7a6028da3bcf713f4019dccc8
kodekloud-terraform-demo-user <96228864+kodekloud-terraform-demo-user@users.noreply.github.com>
2021-12-16 20:30:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable "secret_key" {}
 resource "aws_instance" "terraform-cloud-demo-server" {
   ami           = var.ami
   instance_type = var.instance_type
+  subnet_id   = data.aws_subnet.subnet1.id
   tags = {
    Name = "terraform-cloud-demo-server" 
   }
