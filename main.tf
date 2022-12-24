654c16a9403bab753aa11ef052b52eb1311be176
Yemi Lawal <33167722+yemveiser@users.noreply.github.com>
2022-12-24 04:04:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ resource "aws_instance" "terraform-cloud-New-Version" {
   instance_type = var.instance_type
   subnet_id   = var.subnet_id
   tags = {
-   Name = "terraform-cloud-demo-server" 
+   Name = "terraform-cloud-New-Version" 
   }
 }
